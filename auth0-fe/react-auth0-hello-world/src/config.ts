c2bac58d1680efa8cb71679d9c305ccb4d851624
Paresh Chouhan <paresh@cumul.io>
2023-01-30 15:48:53 +0530

    Chore: Remove all placeholder values

--- auth0-fe/react-auth0-hello-world/src/config.ts
+++ auth0-fe/react-auth0-hello-world/src/config.ts
@@ -1,7 +1,7 @@
 const config = {
-  domain: 'dev-mxdu54vq.us.auth0.com',
-  clientId: 'tR0pmh3WR5hGBCzaEZ9kHy2PqoG17jFz',
-  audience: 'https://dev-mxdu54vq.us.auth0.com/api/v2/'
+  domain: 'AUTH_DOMAIN_HERE',
+  clientId: 'AUTH_CLIENT_ID_HERE',
+  audience: 'AUTH_AUDIENCE_HERE'
 }
 
 export default config;
